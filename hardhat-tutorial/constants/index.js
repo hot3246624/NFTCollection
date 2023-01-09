3c57d49b3dfcbeb6e5d79012225e938603bdf53d
huangzhiyong <hot3246624@163.com>
2023-01-09 19:33:29 +0800

    new contact

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xCF9d9EA27B8f4455d15E7D3707F981B10D3E36BA";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-nine-navy.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
